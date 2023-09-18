bd6a04dcd562ee5a86ac8d1d1f0baf5867bc2a97
Gianmarco Moretti <gianmarco.moretti@doit.it>
2023-09-18 11:35:54 +0200

    feat: Application setup

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+// public/service-worker.js
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-pwa-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                // Add other assets you want to cache
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
