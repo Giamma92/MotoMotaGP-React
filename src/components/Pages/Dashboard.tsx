8a984c8f82ee269c535d5939cbc26ca525a4186e
Gianmarco Moretti <gianmarco.moretti@doit.it>
2023-09-23 16:10:17 +0200

    feat: first draft of dashboard

---
+++ src/components/Pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+
+import LeftMenu from 'components/UI/LeftMenu';
+import Tiles from 'components/UI/Tiles';
+
+const Dashboard = () => {
+    return (
+        <div className="flex">
+            <LeftMenu />
+            <Tiles />
+        </div>
+    );
+};
+
+export default Dashboard;
